c535e0f17a70f6e520d91215f06870e834fbccbf
Shomari <nyusternie@sdot.io>
2025-10-04 14:59:48 -0400

    Update icon.

--- src/components/SidebarNav.vue
+++ src/components/SidebarNav.vue
@@ -83,9 +83,7 @@
         <!-- Disconnect Identity -->
         <div class="border-t border-slate-700 pt-4">
             <button @click="handleDisconnect" class="w-full flex items-center gap-3 px-4 py-2 rounded-lg text-red-400 hover:bg-slate-700 hover:text-white transition-colors">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
-                </svg>
+                <ArrowLeftStartOnRectangleIcon class="size-5" />
 
                 <span>
                     Disconnect
@@ -103,6 +101,7 @@ import { invoke } from '@tauri-apps/api/core'
 
 import {
     AdjustmentsHorizontalIcon,
+    ArrowLeftStartOnRectangleIcon,
     BookmarkSquareIcon,
     HashtagIcon,
     MagnifyingGlassIcon,
