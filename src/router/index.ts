7de5fa1e35d7066ff854730dc170443feb9704ab
Shomari <nyusternie@sdot.io>
2025-10-04 15:14:28 -0400

    Add Premium SANS unlock.

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,14 +4,21 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 /* Import your layout and pages. */
 import AppLayout from '../layouts/AppLayout.vue'
 import Home from '../pages/Home.vue'
-import Explorer from '../pages/Explorer.vue'
-import Posts from '../pages/Posts.vue'
-import Community from '../pages/Community.vue'
+
 import Apps from '../pages/Apps.vue'
-import Wallet from '../pages/Wallet.vue'
+import Bootstrap from '../pages/Bootstrap.vue'
+import Community from '../pages/Community.vue'
+import Explorer from '../pages/Explorer.vue'
 import Favorites from '../pages/Favorites.vue'
 import Identities from '../pages/Identities.vue'
+import Posts from '../pages/Posts.vue'
 import Settings from '../pages/Settings.vue'
+import Wallet from '../pages/Wallet.vue'
+
+/* Set Premium SANS Identity flag. */
+// NOTE: FIVE HUNDRED (500) STAKED SANS IS REQUIRED
+//       to unlock early access to PREMIUM features.
+const PREMIUM_SANS_IDENTITY = true
 
 /* Initialize routes. */
 const routes = [
@@ -20,14 +27,15 @@ const routes = [
         component: AppLayout, // NOTE: layout wraps all pages
         children: [
             { path: '', component: Home }, // NOTE: default child route
-            { path: 'explorer', component: Explorer },
-            { path: 'posts', component: Posts },
-            { path: 'community', component: Community },
-            { path: 'apps', component: Apps },
-            { path: 'wallet', component: Wallet },
-            { path: 'favorites', component: Favorites },
+            { path: 'apps', component: PREMIUM_SANS_IDENTITY ? Favorites : Apps },
+            { path: 'bootstrap', component: Bootstrap },
+            { path: 'community', component: PREMIUM_SANS_IDENTITY ? Community : Bootstrap },
+            { path: 'explorer', component: PREMIUM_SANS_IDENTITY ? Explorer : Bootstrap },
+            { path: 'favorites', component: PREMIUM_SANS_IDENTITY ? Favorites : Bootstrap },
             { path: 'identities', component: Identities },
+            { path: 'posts', component: PREMIUM_SANS_IDENTITY ? Posts : Bootstrap },
             { path: 'settings', component: Settings },
+            { path: 'wallet', component: Wallet },
         ]
     }
 ]
