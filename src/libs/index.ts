36f98545c1ce90f0d1353009498deb5298808201
Shomari <nyusternie@sdot.io>
2026-01-01 02:11:45 -0500

    Cleanup unused.

--- src/libs/index.ts
+++ src/libs/index.ts
@@ -1,11 +1,8 @@
 // src/libs/index.ts
 
-export { default as getNetwork } from './getNetwork'
 export { default as getMnemonic } from './getMnemonic'
-export { default as getLicense } from './getLicense'
 export { default as getPrivateKeys } from './getPrivateKeys'
 export { getPrivateKeyManager } from './keys/PrivateKeyManager'
-export { default as getTransferKey } from './getTransferKey'
 export { default as sendCredit } from './sendCredit'
 export { default as sendToken } from './sendToken'
 
